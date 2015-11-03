ac1644dfae18c712d5b35f0adbd5ff3a0e31b595
Michel Albers <Michel.Albers@sophos.com>
2015-11-03 10:59:08 +0100

    Remove documentation:null from package.js

--- package.js
+++ package.js
@@ -1,13 +1,8 @@
 Package.describe({
   name: 'interwebs:image-size',
-  version: '1.0.0',
-  // Brief, one-line summary of the package.
+  version: '1.0.2',
   summary: 'Retrieve the size of a remotely hosted image.',
-  // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/michelalbers/meteor-imagesize',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: null
+  git: 'https://github.com/michelalbers/meteor-imagesize'
 });
 
 Npm.depends({
